f36ff3ea4c67d6fbe5d42eff39de35d299a5f296
Sean Rowan <seanrowan@zipcoders-MacBook-Pro.local>
2019-02-09 11:59:25 -0500

    added basic comp methods

--- Display.java
+++ Display.java
@@ -89,8 +89,27 @@ public class Display {
         this.currentDisplay = 0;
     }
     
-    public double add(double d1, double d2){
-        return d1 + d2;
+    public double add(double x, double y){
+        return x + y;
     }
     
+    public double subtract(double x, double y){
+        return x - y;
+    }
+    
+    public double multiply(double x, double y){
+        return x * y;
+    } 
+    
+    public double divide(double x, double y){
+        return x / y;
+    }
+    
+    public double modulous(double x, double y){
+        return x % y;
+    }
+    
+    public double exponent(double x, double y){
+        return Math.pow(x, y);
+    }
 }
--- MainApplication.java
+++ MainApplication.java
@@ -1,32 +1,49 @@
 public class MainApplication {
-    
-    
+
     public static void main(String[] args) {
-        
         boolean run = true;
         Display display = new Display();
         display.println("TIME TO CALCULATE!");
-        
+
         while (run){
-            
-        double x = display.getDoubleInput("Enter a number");
-        String s = display.getStringInput("Enter an operator");
-        double y = display.getDoubleInput("Enter another number");
-        double result;
-        
-        switch(s){
-            case "+" : result = display.add(x, y);
-            break;
-            default : result = 0;
+
+            double x = display.getDoubleInput("Enter a number");
+            String s = display.getStringInput("Enter an operator");
+            double y = display.getDoubleInput("Enter another number");
+            double result;
+
+            switch(s){
+                case "+" : result = display.add(x, y);
+                break;
+
+                case "-" : result = display.subtract(x, y);
+                break;
+
+                case "/" : result = display.divide(x, y);
+                break;
+
+                case "*" : result = display.multiply(x, y);
+                break;
+
+                case "%" : result = display.modulous(x, y);
+                break;
+
+                case "^" : result = display.exponent(x, y);
+                break;
+
+
+                default : result = 0;
+                break;
+
+            }
+            display.println("%f %s %f = %f", x, s, y, result);
+
+            String quit = display.getStringInput("Enter 'quit' to stop");
+
+            if (quit.equals("quit")){
+                run = false;
+
+            }
         }
-        display.println("%f %s %f = %f", x, s, y, result);
-        
-        String quit = display.getStringInput("Enter 'quit' to stop");
-        
-        if (quit.equals("quit")){
-            run = false;
-        
-        }
-    }
     }
 }
