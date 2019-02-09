68955ad70c7be67f65c0954c87a57ece85279bee
Cara Eppes <caraeppes@zipcoders-MBP-4.fios-router.home>
2019-02-09 00:37:16 -0500

    created a Display class with a I/O functonality

---
+++ Display.java
@@ -0,0 +1,72 @@
+
+/**
+ * Write a description of class Display here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class Display {
+    
+    private final Scanner scanner;
+    private final PrintStream out;
+
+    public Display() {
+        this(System.in, System.out);
+    }
+
+    public Display(InputStream inputStream, OutputStream outputStream) {
+        this(new Scanner(inputStream), new PrintStream(outputStream));
+    }
+
+    public Display(Scanner scanner, PrintStream printStream) {
+        this.scanner = scanner;
+        this.out = printStream;
+    }
+    
+     /**
+     * @param val  : text to display on console
+     * @param args : optional arguments to send for string formatting
+     */
+    public void print(String val, Object... args) {
+        out.format(val, args);
+    }
+
+    /**
+     * @param val  : text to display on console
+     * @param args : optional arguments to send for string formatting
+     */
+    public void println(String val, Object... args) {
+        out.format(val + "\n", args);
+    }
+    
+    public Scanner getScanner(){
+        return this.scanner;
+    }
+    
+       /**
+     * @param prompt : text to display to user
+     * @param args   : optional arguments to send for string formatting
+     * @return user's input as double
+     */
+    public Double getDoubleInput(String prompt, Object... args) {
+        println(prompt);
+        return this.scanner.nextDouble();
+    }
+    
+    
+     /**
+     * @param prompt : text to display to user
+     * @param args   : optional arguments to send for string formatting
+     * @return user's input as String
+     */
+    public String getStringInput(String prompt, Object... args) {
+        println(prompt);
+        return this.scanner.next();
+    }
+}
--- MainApplication.java
+++ MainApplication.java
@@ -1,4 +1,19 @@
 public class MainApplication {
+    
+    
     public static void main(String[] args) {
+        
+        Display display = new Display();
+        display.println("TIME TO CALCULATE!");
+        double x = display.getDoubleInput("Enter a number");
+        String s = display.getStringInput("Enter an operator");
+        double y = display.getDoubleInput("Enter another number");
+        
+        
+        
+        
+        
+        
+        
     }
 }
